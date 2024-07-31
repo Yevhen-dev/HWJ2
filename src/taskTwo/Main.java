250f7f1fa04bcc6e983bb116d4f3cdb767bb19ab
Yevhen-dev <chuharkin2010@gmail.com>
2024-07-31 10:10:21 +0100

    changed mistake with 10th floor

--- src/taskTwo/Main.java
+++ src/taskTwo/Main.java
@@ -12,13 +12,13 @@ public class Main {
 		long flatNum = sc.nextLong();
 		
 		if( flatNum >= 1 && flatNum <= 36 ) {
-				System.out.println( "Flat #" + flatNum + " is in a first hall and on a " + ( flatNum/4 + 1 ) + " floor");
+				System.out.println( "Flat #" + flatNum + " is in a first hall and on a " + ( flatNum == 36 ? flatNum/4 : flatNum/4 + 1 ) + " floor");
 		} else if( flatNum >= 37 && flatNum <= 72 ) {
-			System.out.println( "Flat #" + flatNum + " is in a second hall and on a " + ( ( flatNum - 36 )/4 + 1 ) + " floor");
+			System.out.println( "Flat #" + flatNum + " is in a second hall and on a " + ( flatNum == 72 ? ( flatNum-36 )/4 : ( flatNum - 36 )/4 + 1 ) + " floor");
 		} else if( flatNum >= 73 && flatNum <= 108 ) {
-			System.out.println( "Flat #" + flatNum + " is in a third hall and on a " + ( ( flatNum - 72 )/4 + 1 ) + " floor");
+			System.out.println( "Flat #" + flatNum + " is in a third hall and on a " + ( flatNum == 108 ? ( flatNum - 72 )/4 : ( flatNum - 72 )/4 + 1 ) + " floor");
 		} else if( flatNum >= 109 && flatNum <= 144 ) {
-			System.out.println( "Flat #" + flatNum + " is in a fourth hall and on a " + ( ( flatNum - 108 )/4 + 1 ) + " floor");
+			System.out.println( "Flat #" + flatNum + " is in a fourth hall and on a " + ( flatNum == 144 ? (flatNum - 108)/4 : ( flatNum - 108 )/4 + 1 ) + " floor");
 		} else {
 			System.out.println("Type correct number");
 		}
